9f77d282ce72d3ae3f57a045822d3e1abcc87c20
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-11-16 10:06:28 -0300

    Setup Piece + Background Colors + Font Colors

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Board : MonoBehaviour
 {
+    public GameObject piece;
+
     public RectTransform corner;
 
-    public Color[] colors;
+    public Color[] pieceBackgroundColors;
+    public Color[] pieceFontColors;
+
+    public int gridWidth;
+    public int gridHeight;
 
     private Vector2 _pieceSize;
     private Vector2 _cornerCenterPos;
+
+    private GameObject[,] _grid;
 
     // Start is called before the first frame update
     private void Start()
     {
         _pieceSize = corner.sizeDelta;
         _cornerCenterPos = corner.anchoredPosition;
+
+        _grid = new GameObject[gridWidth, gridHeight];
     }
 }
