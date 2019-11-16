569c37ea730a9ad492fad3b6d6340dfcdfd36d79
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-11-16 10:36:24 -0300

    Spawn initial pieces

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.Serialization;
 
 public class Board : MonoBehaviour
@@ -12,6 +13,7 @@ public class Board : MonoBehaviour
     public Color[] pieceBackgroundColors;
     public Color[] pieceFontColors;
 
+    public int gridThickness;
     public int gridWidth;
     public int gridHeight;
 
@@ -27,5 +29,28 @@ public class Board : MonoBehaviour
         _cornerCenterPos = corner.anchoredPosition;
 
         _grid = new GameObject[gridWidth, gridHeight];
+
+        var first = Instantiate(piece, transform);
+        var second = Instantiate(piece, transform);
+
+        var firstGridPos = new Vector2(Random.Range(0, gridWidth), Random.Range(0, gridHeight));
+
+        var secondGridPos = firstGridPos;
+        while (secondGridPos == firstGridPos)
+        {
+            secondGridPos = new Vector2(Random.Range(0, gridWidth), Random.Range(0, gridHeight));
+        }
+
+        var firstAnchoredPos = GridPosToBoardPos(firstGridPos);
+        var secondAnchoredPos = GridPosToBoardPos(secondGridPos);
+
+        first.GetComponent<RectTransform>().anchoredPosition = firstAnchoredPos;
+        second.GetComponent<RectTransform>().anchoredPosition = secondAnchoredPos;
+    }
+
+    private Vector2 GridPosToBoardPos(Vector2 gridPos)
+    {
+        return _cornerCenterPos +
+               Vector2.Scale(gridPos, _pieceSize + new Vector2(gridThickness, gridThickness));
     }
 }
