e464a963b9613a3236460516923d91038c51e7fc
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-12-01 00:17:27 -0300

    Reload

---
+++ Assets/Reload.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Reload : MonoBehaviour
+{
+    private Button button;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("SampleScene");
+        });
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
